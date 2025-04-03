40c2d465f8e52170e9eb2a898901bad6025bd64b
ErikDaSilva19 <erikborgesdasilva999@gmail.com>
2025-04-03 10:20:23 -0300

    ajuste na conexao do serverJs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${port}`);
