4254a814ee8b5e7a1ba733d350a0f8aa8024080b
ErikDaSilva19 <erikborgesdasilva999@gmail.com>
2025-04-04 10:17:15 -0300

    Alterdo para versoa anterior

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,15 +1,15 @@
 import multer from 'multer';
 import { v4 as uuidv4 } from 'uuid';
 import { dirname, extname, resolve } from 'node:path';
-import { fileURLToPath } from 'url'; // Importando para corrigir o __dirname
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename); // Definindo __dirname manualmente
+const __dirname = dirname(__filename);
 
 export default {
   storage: multer.diskStorage({
     destination: (req, file, callback) => {
-      callback(null, resolve(__dirname, '..', '..', 'uploads')); // Usando __dirname corretamente
+      callback(null, resolve(__dirname, '..', '..', 'uploads'));
     },
     filename: (req, file, callback) => {
       callback(null, `${uuidv4()}${extname(file.originalname)}`);
